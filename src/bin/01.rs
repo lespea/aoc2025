4202af9ff323800336b2a32e2597dddf9168e4dc
Adam Lesperance <lespea@gmail.com>
2025-12-03 00:46:46 -0600

    Solve 1.2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,11 +5,12 @@ use anyhow::{Context, bail};
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut pos = 50;
+    let mut pos = 50u16;
     let mut zeros = 0;
 
     for line in input.lines() {
-        pos = Move::try_from(line).unwrap().spin(pos);
+        let res = Move::try_from(line).unwrap().spin(pos);
+        pos = res.pos;
         if pos == 0 {
             zeros += 1;
         }
@@ -19,7 +20,18 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut pos = 50;
+    let mut zeros = 0;
+
+    for line in input.lines() {
+        let m = Move::try_from(line).unwrap();
+        let res = m.spin(pos);
+        pos = res.pos;
+        zeros += res.zeros;
+        // dbg!(m, res, zeros);
+    }
+
+    Some(zeros)
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -34,15 +46,60 @@ struct Move {
     steps: NonZero<u16>,
 }
 
-impl Move {
-    fn spin(&self, mut pos: i16) -> i16 {
-        match self.dir {
-            Dir::Left => pos -= self.steps.get() as i16,
-            Dir::Right => pos += self.steps.get() as i16,
-        }
+#[derive(Debug, Clone, Copy)]
+struct SpinResults {
+    pos: u16,
+    zeros: u64,
+}
 
-        pos %= 100;
-        if pos < 0 { 100 + pos } else { pos }
+impl Move {
+    fn spin(&self, pos: u16) -> SpinResults {
+        let steps = self.steps.get();
+
+        let mut zeros = (steps / 100) as u64;
+        let steps = steps % 100;
+
+        let diff = match self.dir {
+            Dir::Left => -(steps as i16),
+            Dir::Right => steps as i16,
+        };
+
+        let final_pos = self.resolve((pos as i16) + diff, pos == 0, &mut zeros);
+
+        SpinResults {
+            pos: final_pos,
+            zeros,
+        }
+    }
+
+    #[inline]
+    fn resolve(&self, pos: i16, start_zero: bool, zeros: &mut u64) -> u16 {
+        if pos == 0 {
+            if !start_zero {
+                *zeros += 1;
+            }
+            0
+        } else if pos > 0 {
+            if pos >= 100 {
+                *zeros += 1;
+                (pos - 100) as u16
+            } else {
+                pos as u16
+            }
+        } else {
+            if !start_zero {
+                *zeros += 1;
+            }
+
+            let pos = if pos <= -100 {
+                *zeros += 1;
+                pos + 100
+            } else {
+                pos
+            };
+
+            if pos < 0 { (100 + pos) as u16 } else { 0 }
+        }
     }
 }
 
@@ -79,7 +136,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 
     #[test]
@@ -97,7 +154,8 @@ mod tests {
                     dir: spin.0,
                     steps: NonZero::<u16>::new(spin.1).unwrap()
                 }
-                .spin(spin.2),
+                .spin(spin.2)
+                .pos,
                 spin.3
             );
         }
